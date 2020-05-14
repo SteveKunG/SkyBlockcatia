d582bb4b7318df2533f445aa02ad349db338a707
SteveKunG <mccommander_minecraft@hotmail.com>
2020-05-14 22:39:53 +0700

    Fixed skin rendering

--- src/main/java/com/stevekung/skyblockcatia/mixin/MixinDownloadingTexture.java
+++ src/main/java/com/stevekung/skyblockcatia/mixin/MixinDownloadingTexture.java
@@ -1,9 +1,8 @@
 package com.stevekung.skyblockcatia.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 import com.stevekung.skyblockcatia.config.SkyBlockcatiaConfig;
 
@@ -25,16 +24,60 @@ public abstract class MixinDownloadingTexture
         throw new Error();
     }
 
-    @Redirect(method = "processLegacySkin(Lnet/minecraft/client/renderer/texture/NativeImage;)Lnet/minecraft/client/renderer/texture/NativeImage;", at = @At(value = "INVOKE", target = "net/minecraft/client/renderer/texture/DownloadingTexture.setAreaOpaque(Lnet/minecraft/client/renderer/texture/NativeImage;IIII)V", ordinal = 0))
-    private static void fixSkinRendering(NativeImage image, int x, int y, int width, int height)
+    @SuppressWarnings("resource")
+    @Overwrite
+    private static NativeImage processLegacySkin(NativeImage nativeImage)
     {
-        if (SkyBlockcatiaConfig.GENERAL.enableSkinRenderingFix.get())
+        boolean flag = nativeImage.getHeight() == 32;
+
+        if (flag)
         {
-            MixinDownloadingTexture.setAreaTransparent(image, x, y, width, height);
+            NativeImage nativeimage = new NativeImage(64, 64, true);
+            nativeimage.copyImageData(nativeImage);
+            nativeImage.close();
+            nativeImage = nativeimage;
+            nativeimage.fillAreaRGBA(0, 32, 64, 32, 0);
+            nativeimage.copyAreaRGBA(4, 16, 16, 32, 4, 4, true, false);
+            nativeimage.copyAreaRGBA(8, 16, 16, 32, 4, 4, true, false);
+            nativeimage.copyAreaRGBA(0, 20, 24, 32, 4, 12, true, false);
+            nativeimage.copyAreaRGBA(4, 20, 16, 32, 4, 12, true, false);
+            nativeimage.copyAreaRGBA(8, 20, 8, 32, 4, 12, true, false);
+            nativeimage.copyAreaRGBA(12, 20, 16, 32, 4, 12, true, false);
+            nativeimage.copyAreaRGBA(44, 16, -8, 32, 4, 4, true, false);
+            nativeimage.copyAreaRGBA(48, 16, -8, 32, 4, 4, true, false);
+            nativeimage.copyAreaRGBA(40, 20, 0, 32, 4, 12, true, false);
+            nativeimage.copyAreaRGBA(44, 20, -8, 32, 4, 12, true, false);
+            nativeimage.copyAreaRGBA(48, 20, -16, 32, 4, 12, true, false);
+            nativeimage.copyAreaRGBA(52, 20, -8, 32, 4, 12, true, false);
+        }
+
+        if (!SkyBlockcatiaConfig.GENERAL.enableSkinRenderingFix.get())
+        {
+            nativeImage.copyAreaRGBA(4, 16, 16, 32, 4, 4, true, false);
+            nativeImage.copyAreaRGBA(8, 16, 16, 32, 4, 4, true, false);
+            nativeImage.copyAreaRGBA(0, 20, 24, 32, 4, 12, true, false);
+            nativeImage.copyAreaRGBA(4, 20, 16, 32, 4, 12, true, false);
+            nativeImage.copyAreaRGBA(8, 20, 8, 32, 4, 12, true, false);
+            nativeImage.copyAreaRGBA(12, 20, 16, 32, 4, 12, true, false);
+            nativeImage.copyAreaRGBA(44, 16, -8, 32, 4, 4, true, false);
+            nativeImage.copyAreaRGBA(48, 16, -8, 32, 4, 4, true, false);
+            nativeImage.copyAreaRGBA(40, 20, 0, 32, 4, 12, true, false);
+            nativeImage.copyAreaRGBA(44, 20, -8, 32, 4, 12, true, false);
+            nativeImage.copyAreaRGBA(48, 20, -16, 32, 4, 12, true, false);
+            nativeImage.copyAreaRGBA(52, 20, -8, 32, 4, 12, true, false);
         }
         else
         {
-            MixinDownloadingTexture.setAreaOpaque(image, x, y, width, height);
+            setAreaOpaque(nativeImage, 0, 0, 32, 16);
+
+            if (flag)
+            {
+                setAreaTransparent(nativeImage, 32, 0, 64, 32);
+            }
+
+            setAreaOpaque(nativeImage, 0, 16, 64, 32);
+            setAreaOpaque(nativeImage, 16, 48, 48, 64);
         }
+        return nativeImage;
     }
 }
