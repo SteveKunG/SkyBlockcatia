8d4d10ce5b718c7b825a0e0ac69092f32d6fe907
SteveKunG <mccommander_minecraft@hotmail.com>
2020-06-15 01:22:57 +0700

    Restore chat depth

--- src/main/java/com/stevekung/skyblockcatia/mixin/GuiNewChatMixin.java
+++ src/main/java/com/stevekung/skyblockcatia/mixin/GuiNewChatMixin.java
@@ -1,17 +1,33 @@
 package com.stevekung.skyblockcatia.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.stevekung.skyblockcatia.core.SkyBlockcatiaMod;
 
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiNewChat;
+import net.minecraft.client.renderer.GlStateManager;
 
 @Mixin(GuiNewChat.class)
 public abstract class GuiNewChatMixin extends Gui
 {
+    @Inject(method = "drawChat(I)V", at = @At("HEAD"))
+    private void renderDepthPre(int updateCounter, CallbackInfo info)
+    {
+        GlStateManager.disableDepth();
+    }
+
+    @Inject(method = "drawChat(I)V", at = @At("RETURN"))
+    private void renderDepthPost(int updateCounter, CallbackInfo info)
+    {
+        GlStateManager.enableDepth();
+    }
+
     @ModifyConstant(method = "drawChat(I)V", constant = @Constant(floatValue = 20.0F))
     private float modifyChatHeight(float defaultValue)
     {
