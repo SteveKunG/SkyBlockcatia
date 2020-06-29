7c275e688f74ca31fb51a0cd3dee890877097108
SteveKunG <mccommander_minecraft@hotmail.com>
2020-06-29 13:19:46 +0700

    Revert compact number format

--- src/main/java/com/stevekung/skyblockcatia/utils/NumberUtils.java
+++ src/main/java/com/stevekung/skyblockcatia/utils/NumberUtils.java
@@ -1,17 +1,44 @@
 package com.stevekung.skyblockcatia.utils;
 
 import java.text.DecimalFormat;
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.TreeMap;
 
 public class NumberUtils
 {
-    public static String formatCompact(long number)
+    private static final NavigableMap<Long, String> SUFFIXES = new TreeMap<>();
+
+    static
     {
-        if (number < 1000)
+        SUFFIXES.put(1_000L, "k");
+        SUFFIXES.put(1_000_000L, "M");
+        SUFFIXES.put(1_000_000_000L, "G");
+        SUFFIXES.put(1_000_000_000_000L, "T");
+        SUFFIXES.put(1_000_000_000_000_000L, "P");
+        SUFFIXES.put(1_000_000_000_000_000_000L, "E");
+    }
+
+    public static String formatCompact(long value)
+    {
+        if (value == Long.MIN_VALUE)
         {
-            return String.valueOf(number);
+            return NumberUtils.formatCompact(Long.MIN_VALUE + 1);
         }
-        int exp = (int) (Math.log(number) / Math.log(1000));
-        return String.format("%.1f%c", number / Math.pow(1000, exp), "kMGTPE".charAt(exp - 1));
+        if (value < 0)
+        {
+            return "-" + NumberUtils.formatCompact(-value);
+        }
+        if (value < 1000)
+        {
+            return Long.toString(value);
+        }
+        Map.Entry<Long, String> entry = NumberUtils.SUFFIXES.floorEntry(value);
+        Long divideBy = entry.getKey();
+        String suffix = entry.getValue();
+        long truncated = value / (divideBy / 10);
+        boolean hasDecimal = truncated < 100 && truncated / 10D != truncated / 10;
+        return hasDecimal ? truncated / 10D + suffix : truncated / 10 + suffix;
     }
 
     public static boolean isNumeric(CharSequence cs)
@@ -34,7 +61,7 @@ public class NumberUtils
 
         for (int i = 0; i < sz; i++)
         {
-            if (!Character.isDigit(cs.charAt(i)) && !(cs.charAt(i) == 'k' || cs.charAt(i) == 'm'))
+            if (!Character.isDigit(cs.charAt(i)) && !(cs.charAt(i) == 'k' || cs.charAt(i) == 'm' || cs.charAt(i) == 'K' || cs.charAt(i) == 'M'))
             {
                 return false;
             }
