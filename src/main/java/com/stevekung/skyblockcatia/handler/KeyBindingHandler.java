d50a43263c5d8bca2e6eedff188dbf545becb61e
SteveKunG <mccommander_minecraft@hotmail.com>
2020-07-25 10:49:11 +0700

    Change default key of API Viewer to F8

--- src/main/java/com/stevekung/skyblockcatia/handler/KeyBindingHandler.java
+++ src/main/java/com/stevekung/skyblockcatia/handler/KeyBindingHandler.java
@@ -19,7 +19,7 @@ public class KeyBindingHandler
     public static void init()
     {
         KeyBindingHandler.KEY_QUICK_CONFIG = new KeyBindingIU("key.quick_config.desc", Keyboard.KEY_F4);
-        KeyBindingHandler.KEY_SB_API_VIEWER = new KeyBindingIU("key.sb_api_viewer.desc", Keyboard.KEY_F6);
+        KeyBindingHandler.KEY_SB_API_VIEWER = new KeyBindingIU("key.sb_api_viewer.desc", Keyboard.KEY_F8);
         KeyBindingHandler.KEY_SB_ENDER_CHEST = new KeyBindingIU("key.sb_ender_chest.desc", Keyboard.KEY_NUMPAD5);
         KeyBindingHandler.KEY_SB_CRAFTED_MINIONS = new KeyBindingIU("key.sb_crafted_minions.desc", Keyboard.KEY_NUMPAD2);
         KeyBindingHandler.KEY_SB_CRAFTING_TABLE = new KeyBindingIU("key.sb_crafting_table.desc", Keyboard.KEY_ADD);
