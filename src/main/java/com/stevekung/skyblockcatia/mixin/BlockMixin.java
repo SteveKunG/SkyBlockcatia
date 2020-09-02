3b45d2d702ec84d6a4864faf37cea41cfa76904f
SteveKunG <mccommander_minecraft@hotmail.com>
2020-09-03 00:18:06 +0700

    Add skyblock check to ignore bush hitbox

--- src/main/java/com/stevekung/skyblockcatia/mixin/BlockMixin.java
+++ src/main/java/com/stevekung/skyblockcatia/mixin/BlockMixin.java
@@ -6,6 +6,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import com.stevekung.skyblockcatia.config.ExtendedConfig;
+import com.stevekung.skyblockcatia.event.HypixelEventHandler;
 
 import net.minecraft.block.*;
 import net.minecraft.util.BlockPos;
@@ -21,7 +22,7 @@ public abstract class BlockMixin
     @Inject(method = "collisionRayTrace(Lnet/minecraft/world/World;Lnet/minecraft/util/BlockPos;Lnet/minecraft/util/Vec3;Lnet/minecraft/util/Vec3;)Lnet/minecraft/util/MovingObjectPosition;", cancellable = true, at = @At("HEAD"))
     private void collisionRayTrace(World world, BlockPos pos, Vec3 start, Vec3 end, CallbackInfoReturnable<MovingObjectPosition> info)
     {
-        if (ExtendedConfig.instance.ignoreBushHitbox)
+        if (ExtendedConfig.instance.ignoreBushHitbox && HypixelEventHandler.isSkyBlock)
         {
             if (this.that instanceof BlockFlower || this.that instanceof BlockTallGrass || this.that instanceof BlockDoublePlant || this.that instanceof BlockMushroom)
             {
