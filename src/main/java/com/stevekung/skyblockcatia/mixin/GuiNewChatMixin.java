8cb331273d9f14eda5bc24a721e70b00ef7c9174
SteveKunG <mccommander_minecraft@hotmail.com>
2020-11-04 00:29:38 +0700

    Remove 1.12 chat

--- src/main/java/com/stevekung/skyblockcatia/mixin/GuiNewChatMixin.java
+++ src/main/java/com/stevekung/skyblockcatia/mixin/GuiNewChatMixin.java
@@ -2,12 +2,8 @@ package com.stevekung.skyblockcatia.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import com.stevekung.skyblockcatia.core.SkyBlockcatiaMod;
 
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiNewChat;
@@ -26,23 +22,5 @@ public abstract class GuiNewChatMixin extends Gui
     private void renderDepthPost(int updateCounter, CallbackInfo info)
     {
         GlStateManager.enableDepth();
-    }
-
-    @ModifyConstant(method = "drawChat(I)V", constant = @Constant(floatValue = 20.0F))
-    private float modifyChatHeight(float defaultValue)
-    {
-        return SkyBlockcatiaMod.isPatcherLoaded ? defaultValue : 8.0F;
-    }
-
-    @ModifyConstant(method = "getChatComponent(II)Lnet/minecraft/util/IChatComponent;", constant = @Constant(intValue = 3))
-    private int modifyChatMouseX(int defaultValue)
-    {
-        return SkyBlockcatiaMod.isPatcherLoaded ? defaultValue : 2;
-    }
-
-    @ModifyConstant(method = "getChatComponent(II)Lnet/minecraft/util/IChatComponent;", constant = @Constant(intValue = 27))
-    private int modifyChatMouseY(int defaultValue)
-    {
-        return SkyBlockcatiaMod.isPatcherLoaded ? defaultValue : 40;
     }
 }
