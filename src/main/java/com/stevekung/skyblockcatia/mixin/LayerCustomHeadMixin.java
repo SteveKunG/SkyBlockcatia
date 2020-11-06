b4833e574cb4e59e907990a96686bad02da4b38a
SteveKunG <mccommander_minecraft@hotmail.com>
2020-11-07 02:16:33 +0700

    Added more baby skin for Glowing Dragon Head

--- src/main/java/com/stevekung/skyblockcatia/mixin/LayerCustomHeadMixin.java
+++ src/main/java/com/stevekung/skyblockcatia/mixin/LayerCustomHeadMixin.java
@@ -39,11 +39,11 @@ public class LayerCustomHeadMixin
     @Redirect(method = "doRenderLayer(Lnet/minecraft/entity/EntityLivingBase;FFFFFFF)V", at = @At(value = "INVOKE", target = "net/minecraft/client/renderer/tileentity/TileEntitySkullRenderer.renderSkull(FFFLnet/minecraft/util/EnumFacing;FILcom/mojang/authlib/GameProfile;I)V"))
     private void renderEnchantedSkull(TileEntitySkullRenderer renderer, float x, float y, float z, EnumFacing facing, float rotation, int meta, GameProfile profile, int p_180543_8_, EntityLivingBase entity, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, float scale)
     {
-        ItemStack itemstack = entity.getCurrentArmor(3);
+        ItemStack itemStack = entity.getCurrentArmor(3);
 
-        if (itemstack != null && itemstack.getItem() != null)
+        if (itemStack != null)
         {
-            TileEntityEnchantedSkullRenderer.INSTANCE.renderSkull(x, y, z, facing, rotation, meta, profile, partialTicks, itemstack.hasEffect(), entity);
+            TileEntityEnchantedSkullRenderer.INSTANCE.renderSkull(x, y, z, facing, rotation, meta, profile, partialTicks, itemStack.hasEffect(), entity);
         }
     }
 
@@ -55,23 +55,12 @@ public class LayerCustomHeadMixin
 
     private void renderGlowingArmor(EntityLivingBase entity, float partialTicks)
     {
-        if (!ExtendedConfig.instance.glowingDragonArmor)
+        ItemStack itemStack = entity.getCurrentArmor(3);
+
+        if (ExtendedConfig.instance.glowingDragonArmor && itemStack != null && itemStack.getItem() == Items.skull && itemStack.hasTagCompound())
         {
-            return;
-        }
-
-        ItemStack itemstack = entity.getCurrentArmor(3);
-
-        if (itemstack != null && itemstack.getItem() == Items.skull)
-        {
-            String texture = "";
-
-            if (!itemstack.hasTagCompound())
-            {
-                return;
-            }
-
-            NBTTagCompound compound = itemstack.getTagCompound().getCompoundTag("ExtraAttributes");
+            String texture = null;
+            NBTTagCompound compound = itemStack.getTagCompound().getCompoundTag("ExtraAttributes");
             String id = compound.getString("id");
 
             if (id.equals("SUPERIOR_DRAGON_HELMET"))
@@ -90,75 +79,135 @@ public class LayerCustomHeadMixin
             }
             else if (id.equals("WISE_DRAGON_HELMET"))
             {
-                texture = "wise";
+                if (compound.hasKey("skin"))
+                {
+                    if (compound.getString("skin").equals("WISE_BABY"))
+                    {
+                        texture = "wise_baby";
+                    }
+                }
+                else
+                {
+                    texture = "wise";
+                }
             }
             else if (id.equals("YOUNG_DRAGON_HELMET"))
             {
-                texture = "young";
+                if (compound.hasKey("skin"))
+                {
+                    if (compound.getString("skin").equals("YOUNG_BABY"))
+                    {
+                        texture = "young_baby";
+                    }
+                }
+                else
+                {
+                    texture = "young";
+                }
             }
             else if (id.equals("OLD_DRAGON_HELMET") || id.equals("PROTECTOR_DRAGON_HELMET"))
             {
-                texture = "white_eye";
+                if (compound.hasKey("skin"))
+                {
+                    if (compound.getString("skin").equals("OLD_BABY"))
+                    {
+                        texture = "old_baby";
+                    }
+                }
+                else if (compound.hasKey("skin"))
+                {
+                    if (compound.getString("skin").equals("PROTECTOR_BABY"))
+                    {
+                        texture = "protector_baby";
+                    }
+                }
+                else
+                {
+                    texture = "white_eye";
+                }
             }
             else if (id.equals("UNSTABLE_DRAGON_HELMET"))
             {
-                texture = "unstable";
+                if (compound.hasKey("skin"))
+                {
+                    if (compound.getString("skin").equals("UNSTABLE_BABY"))
+                    {
+                        texture = "unstable_baby";
+                    }
+                }
+                else
+                {
+                    texture = "unstable";
+                }
             }
             else if (id.equals("STRONG_DRAGON_HELMET"))
             {
-                texture = "strong";
+                if (compound.hasKey("skin"))
+                {
+                    if (compound.getString("skin").equals("STRONG_BABY"))
+                    {
+                        texture = "strong_baby";
+                    }
+                }
+                else
+                {
+                    texture = "strong";
+                }
             }
             else if (id.equals("HOLY_DRAGON_HELMET"))
             {
-                texture = "holy";
+                if (compound.hasKey("skin"))
+                {
+                    if (compound.getString("skin").equals("HOLY_BABY"))
+                    {
+                        texture = "holy_baby";
+                    }
+                }
+                else
+                {
+                    texture = "holy";
+                }
             }
-            else if (id.equals("DIVER_HELMET"))
+
+            if (texture != null)
             {
-                texture = "diver_head";
+                ResourceLocation resource = new ResourceLocation("skyblockcatia:textures/entity/" + texture + ".png");
+                GlStateManager.pushMatrix();
+                GlStateManager.enableBlend();
+                GlStateManager.disableAlpha();
+                GlStateManager.blendFunc(1, 1);
+                GlStateManager.disableLighting();
+                GlStateManager.depthMask(!entity.isInvisible());
+                OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, 128.0F, 128.0F);
+                GlStateManager.enableLighting();
+
+                if (entity.isSneaking())
+                {
+                    GlStateManager.translate(0.0F, 0.2F, 0.0F);
+                }
+
+                this.field_177209_a.postRender(0.0625F);
+                GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+
+                float f3 = 1.1875F;
+                GlStateManager.scale(f3, -f3, -f3);
+                GlStateManager.enableRescaleNormal();
+                GlStateManager.scale(-1.0F, -1.0F, 1.0F);
+                GlStateManager.enableAlpha();
+                Minecraft.getMinecraft().getTextureManager().bindTexture(resource);
+                this.humanoidHead.render(null, 0.0F, 0.0F, 0.0F, 180.0F, 0.0F, 0.0625F);
+
+                int i = entity.getBrightnessForRender(partialTicks);
+                int j = i % 65536;
+                int k = i / 65536;
+                OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, j / 1.0F, k / 1.0F);
+
+                GlStateManager.depthMask(true);
+                GlStateManager.disableBlend();
+                GlStateManager.enableAlpha();
+                GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
+                GlStateManager.popMatrix();
             }
-
-            if (texture.isEmpty())
-            {
-                return;
-            }
-
-            ResourceLocation resource = new ResourceLocation("skyblockcatia:textures/entity/" + texture + ".png");
-
-            GlStateManager.pushMatrix();
-            GlStateManager.enableBlend();
-            GlStateManager.disableAlpha();
-            GlStateManager.blendFunc(1, 1);
-            GlStateManager.disableLighting();
-            GlStateManager.depthMask(!entity.isInvisible());
-            OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, 128.0F, 128.0F);
-            GlStateManager.enableLighting();
-
-            if (entity.isSneaking())
-            {
-                GlStateManager.translate(0.0F, 0.2F, 0.0F);
-            }
-
-            this.field_177209_a.postRender(0.0625F);
-            GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-
-            float f3 = 1.1875F;
-            GlStateManager.scale(f3, -f3, -f3);
-            GlStateManager.enableRescaleNormal();
-            GlStateManager.scale(-1.0F, -1.0F, 1.0F);
-            GlStateManager.enableAlpha();
-            Minecraft.getMinecraft().getTextureManager().bindTexture(resource);
-            this.humanoidHead.render(null, 0.0F, 0.0F, 0.0F, 180.0F, 0.0F, 0.0625F);
-
-            int i = entity.getBrightnessForRender(partialTicks);
-            int j = i % 65536;
-            int k = i / 65536;
-            OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, j / 1.0F, k / 1.0F);
-
-            GlStateManager.depthMask(true);
-            GlStateManager.disableBlend();
-            GlStateManager.enableAlpha();
-            GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
-            GlStateManager.popMatrix();
         }
     }
 }
