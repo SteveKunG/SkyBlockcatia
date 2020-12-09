b0b1570cb2f1a5434d3d6463dc35ec2c0cc96c4d
SteveKunG <mccommander_minecraft@hotmail.com>
2020-12-09 17:38:19 +0700

    Removed 1.15 glint color, Fix incompatible with glint colorizer.

--- src/main/java/com/stevekung/skyblockcatia/mixin/RenderItemMixin.java
+++ src/main/java/com/stevekung/skyblockcatia/mixin/RenderItemMixin.java
@@ -2,12 +2,9 @@ package com.stevekung.skyblockcatia.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import com.stevekung.skyblockcatia.config.SkyBlockcatiaConfig;
 import com.stevekung.skyblockcatia.config.SkyBlockcatiaSettings;
 import com.stevekung.skyblockcatia.utils.RenderUtils;
 
@@ -24,11 +21,5 @@ public class RenderItemMixin
         {
             RenderUtils.renderRarity(itemStack, xPosition, yPosition);
         }
-    }
-
-    @ModifyConstant(method = "renderEffect(Lnet/minecraft/client/resources/model/IBakedModel;)V", constant = @Constant(intValue = -8372020))
-    private int setGlintColor(int defaultValue)
-    {
-        return SkyBlockcatiaConfig.enable1_15ArmorEnchantedGlint ? -7383333 : defaultValue;
     }
 }
