a27f5ec50d95a9dc63c6d8b17c9f09e85de552d5
SteveKunG <mccommander_minecraft@hotmail.com>
2021-06-23 13:17:00 +0700

    Update to loom 0.7.2 and fix hurtTime not remapped

--- common/src/main/java/com/stevekung/skyblockcatia/mixin/player/MixinLocalPlayer.java
+++ common/src/main/java/com/stevekung/skyblockcatia/mixin/player/MixinLocalPlayer.java
@@ -1,17 +1,17 @@
 package com.stevekung.skyblockcatia.mixin.player;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import com.stevekung.skyblockcatia.event.handler.SkyBlockEventHandler;
 import net.minecraft.client.player.LocalPlayer;
 
 @Mixin(LocalPlayer.class)
 public class MixinLocalPlayer
 {
-    @Redirect(method = "hurtTo(F)V", at = @At(value = "FIELD", target = "net/minecraft/client/player/LocalPlayer.hurtTime:I"))
-    private void setNoHurtTime(LocalPlayer entity, int oldValue)
+    @ModifyConstant(method = "hurtTo(F)V", constant = @Constant(intValue = 10, ordinal = 1))
+    private int noHurt(int defValue)
     {
-        entity.hurtTime = SkyBlockEventHandler.isSkyBlock ? 0 : oldValue;
+        return SkyBlockEventHandler.isSkyBlock ? 0 : defValue;
     }
 }
