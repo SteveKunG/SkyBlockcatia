0b078593078bd6824bf5b75e58935011231ab93f
Wasinthorn Suksri <mccommander_minecraft@hotmail.com>
2022-05-01 17:59:04 +0700

    Update buildscript

---
+++ setup.bat
@@ -0,0 +1,5 @@
+:: INTERNAL SETUP, DO NOT USE THIS!
+set GRADLE_USER_HOME=%gradle_ex%
+set JAVA_HOME=C:\Program Files\Eclipse Adoptium\jdk-8.0.312.7-hotspot
+PATH=%JAVA_HOME%
+gradlew setupDecompWorkspace eclipse
